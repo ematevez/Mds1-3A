be842c6828e164327d9d32ab3f2e44fe07eff95d
Gonzalo <agustinlamas2011@gmail.com>
2025-06-10 14:52:15 -0300

    Hola

--- temperatura.py
+++ temperatura.py
@@ -2,3 +2,7 @@ def convertir_a_fahrenheit(celsius):
     return celsius * 33.8  # ERROR: fórmula incorrecta, debería ser *9/5 + 32
 
 print(convertir_a_fahrenheit(0))  # Esperado: 32
+def convertir_a_fahrenheit(celsius):
+    return celsius * 33.8  # ERROR: fórmula incorrecta, debería ser *9/5 + 32
+
+print(convertir_a_fahrenheit(0))  # Esperado: 32
